68a30fdcd06684d8a8d0e9e63f83f177755c6e09
sarwar-asik <sarwarasik@gmail.com>
2023-12-19 03:52:34 +0600

    milestone done

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-      domains: ["img.freepik.com", "i.ibb.co", "i.pinimg.com", "ibb.co","images.unsplash.com","localhost","gw.alipayobjects.com"],
+      domains: ["img.freepik.com", "i.ibb.co", "i.pinimg.com", "ibb.co","images.unsplash.com","localhost","gw.alipayobjects.com",'example.com'],
     },
     // swcMinify:true
     // fastRefresh:true,
---
+++ src/app/(dashboard)/admin/milestone/page.tsx
@@ -0,0 +1,257 @@
+"use client";
+import ActionBar from "@/components/ui/ActionBar";
+import UMBreadCrumb from "@/components/ui/UMBreadCrumb";
+import { Button, Input, message } from "antd";
+import Link from "next/link";
+import {
+  DeleteOutlined,
+  EditOutlined,
+  FilterOutlined,
+  ReloadOutlined,
+  EyeOutlined,
+} from "@ant-design/icons";
+import { useState } from "react";
+import { useDebounced } from "@/redux/hooks";
+import UMTable from "@/components/ui/UMTable";
+
+import dayjs from "dayjs";
+import UMModal from "@/components/ui/UMModal";
+
+import Image from "next/image";
+import {
+  Error_model_hook,
+  Success_model,
+  confirm_modal,
+} from "@/utils/modalHook";
+import {
+  useDeleteCategoryMutation,
+  useGetAllCategoryQuery,
+} from "@/redux/api/adminApi/categoryApi";
+import { USER_ROLE } from "@/constants/role";
+import { useGetAllMilestoneQuery } from "@/redux/api/adminApi/milestoneApi";
+
+const MileStoneList = () => {
+  const query: Record<string, any> = {};
+
+  // const SUPER_ADMIN=USER_ROLE.ADMIN
+
+  const [deleteCategory] = useDeleteCategoryMutation();
+
+  const [page, setPage] = useState<number>(1);
+  const [size, setSize] = useState<number>(10);
+  const [sortBy, setSortBy] = useState<string>("");
+  const [sortOrder, setSortOrder] = useState<string>("");
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [open, setOpen] = useState<boolean>(false);
+  const [adminId, setAdminId] = useState<string>("");
+
+  query["limit"] = size;
+  query["page"] = page;
+  query["sortBy"] = sortBy;
+  query["sortOrder"] = sortOrder;
+  query["status"] = "active";
+
+  const debouncedSearchTerm = useDebounced({
+    searchQuery: searchTerm,
+    delay: 600,
+  });
+
+  if (!!debouncedSearchTerm) {
+    query["searchTerm"] = debouncedSearchTerm;
+  }
+  const { data = [], isLoading } = useGetAllMilestoneQuery({ ...query });
+  console.log(data);
+
+  //@ts-ignore
+  const categoryData = data?.data;
+
+  //@ts-ignore
+  const meta = data?.meta;
+
+  const handleDelete = (id: string) => {
+    confirm_modal(`Are you sure you want to delete`).then(async (res) => {
+      if (res.isConfirmed) {
+        try {
+          console.log(id);
+
+          const res = await deleteCategory(id).unwrap();
+
+          console.log(res, "response for delete category");
+          if (res.success == false) {
+            // message.success("Admin Successfully Deleted!");
+            // setOpen(false);
+            Error_model_hook(res?.message);
+          } else {
+            Success_model("Category Successfully Deleted");
+          }
+        } catch (error: any) {
+          message.error(error.message);
+        }
+      }
+    });
+  };
+
+  const columns = [
+    {
+      title: "",
+      render: function (data: any) {
+        return <>{<Image src={data?.img} width={80} height={50} alt="dd" />}</>;
+      },
+      width: 100,
+    },
+    {
+      title: "Name",
+      dataIndex: "title",
+      ellipsis: true,
+    },
+    {
+      title: "details",
+      dataIndex: "details",
+      ellipsis: true,
+    },
+    {
+      title: "showing_number",
+      dataIndex: "showing_number",
+      ellipsis: true,
+    },
+    {
+      title: "course",
+      dataIndex: "course",
+      ellipsis: true,
+      render: function (data: any) {
+        return <>{data?.title}</>;
+      },
+    },
+    {
+      title: "Created at",
+      dataIndex: "createdAt",
+      render: function (data: any) {
+        return data && dayjs(data).format("MMM D, YYYY hh:mm A");
+      },
+      sorter: true,
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+    },
+    {
+      title: "Action",
+      dataIndex: "_id",
+      render: function (data: any) {
+        return (
+          <>
+            <Link href={`/admin/milestone/details/${data}`}>
+              <Button type="default">
+                <EyeOutlined />
+              </Button>
+            </Link>
+            <Link href={`/admin/milestone/edit/${data}`}>
+              <Button
+                style={{
+                  margin: "0px 5px",
+                }}
+                onClick={() => console.log(data)}
+                type="default"
+              >
+                <EditOutlined />
+              </Button>
+            </Link>
+            <Button onClick={() => handleDelete(data)} type="default" danger>
+              <DeleteOutlined />
+            </Button>
+          </>
+        );
+      },
+    },
+  ];
+  const onPaginationChange = (page: number, pageSize: number) => {
+    console.log("Page:", page, "PageSize:", pageSize);
+    setPage(page);
+    setSize(pageSize);
+  };
+  const onTableChange = (pagination: any, filter: any, sorter: any) => {
+    const { order, field } = sorter;
+    // console.log(order, field);
+    setSortBy(field as string);
+    setSortOrder(order === "ascend" ? "asc" : "desc");
+  };
+
+  const resetFilters = () => {
+    setSortBy("");
+    setSortOrder("");
+    setSearchTerm("");
+  };
+
+  const deleteAdminHandler = async (id: string) => {
+    // console.log(id);
+    try {
+      const res = await deleteCategory(id);
+      if (res) {
+        message.success("Admin Successfully Deleted!");
+        setOpen(false);
+      }
+    } catch (error: any) {
+      message.error(error.message);
+    }
+  };
+
+  return (
+    <div>
+      {/* <UMBreadCrumb
+        items={[
+          {
+            label: "admin",
+            link: "/admin",
+          },
+        ]}
+      /> */}
+      <ActionBar title="Milestone List">
+        <Input
+          size="large"
+          placeholder="Search"
+          onChange={(e) => setSearchTerm(e.target.value)}
+          style={{
+            width: "20%",
+          }}
+        />
+        <div>
+          <Link href={`/admin/milestone/create`}>
+            <Button type="default">Create Category</Button>
+          </Link>
+          {(!!sortBy || !!sortOrder || !!searchTerm) && (
+            <Button
+              style={{ margin: "0px 5px" }}
+              type="default"
+              onClick={resetFilters}
+            >
+              <ReloadOutlined />
+            </Button>
+          )}
+        </div>
+      </ActionBar>
+
+      <UMTable
+        loading={isLoading}
+        columns={columns}
+        dataSource={categoryData}
+        pageSize={size}
+        totalPages={meta?.total}
+        showSizeChanger={true}
+        onPaginationChange={onPaginationChange}
+        onTableChange={onTableChange}
+        showPagination={true}
+      />
+
+      <UMModal
+        title="Remove admin"
+        isOpen={open}
+        closeModal={() => setOpen(false)}
+        handleOk={() => deleteAdminHandler(adminId)}
+      >
+        <p className="my-5">Do you want to remove this admin?</p>
+      </UMModal>
+    </div>
+  );
+};
+
+export default MileStoneList;
