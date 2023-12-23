899bd295b4b71aa527b4e7fddbd0aac09e90207e
sarwar-asik <sarwarasik@gmail.com>
2023-12-23 17:10:27 +0600

    table_border

--- next.config.js
+++ next.config.js
@@ -1,13 +1,47 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      domains: ["img.freepik.com", "i.ibb.co", "i.pinimg.com", "ibb.co","images.unsplash.com","localhost","gw.alipayobjects.com",'example.com'],
-    },
-    // swcMinify:true
-    // fastRefresh:true,
-    // concurrentFeatures:true
+  reactStrictMode: true,
+  // images: {
+  //   domains: ["img.freepik.com", "i.ibb.co", "i.pinimg.com", "ibb.co","images.unsplash.com","localhost","gw.alipayobjects.com",'example.com'],
+  // },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.freepik.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'gw.alipayobjects.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        pathname: '**',
+      },
+    ],
+  },
+  // swcMinify:true
+  // fastRefresh:true,
+  // concurrentFeatures:true
 
-  };
-  
-  module.exports = nextConfig;
+};
+
+module.exports = nextConfig;
