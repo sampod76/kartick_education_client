440f5efa7fd042c8667708dfde9f3e9b81a323da
sarwar-asik <sarwarasik@gmail.com>
2023-12-25 17:30:06 +0600

    profile_complete_student

--- src/app/(dashboard)/admin/manage-users/all-users/details/[id]/page.tsx
+++ src/app/(dashboard)/admin/manage-users/all-users/details/[id]/page.tsx
@@ -1,22 +1,12 @@
 "use client";
 
 import LoadingForDataFetch from "@/components/Utlis/LoadingForDataFetch";
-import { NO_IMAGE } from "@/constants/filePatch";
-import { useGetSingleadminQuery } from "@/redux/api/adminApi";
-import { useGetSingleGeneralUserQuery } from "@/redux/api/adminApi/userManageApi";
-import Image from "next/image";
-import {
-  GithubFilled,
-  TwitterSquareFilled,
-  FacebookFilled,
-  LinkedinFilled,
-  InstagramFilled,
-} from "@ant-design/icons";
-import Link from "next/link";
-import { useGetSingleUserQuery } from "@/redux/api/adminApi/usersApi";
-import ImageTag from "@/components/ui/CustomTag/ImageTag";
 
-const EditUserPage = ({ params }: any) => {
+import { useGetSingleUserQuery } from "@/redux/api/adminApi/usersApi";
+
+import ProfilePage from "@/components/profile/UserProfilePage";
+
+const UserDetailsPage = ({ params }: any) => {
   const id = params.id;
   console.log(id);
   const { data: userData, isLoading: loading } = useGetSingleUserQuery(id);
@@ -27,129 +17,14 @@ const EditUserPage = ({ params }: any) => {
     return <LoadingForDataFetch />;
   }
 
-  const img = userData[userData.role]["img"];
-  const gender = userData[userData.role]["gender"];
-  const phoneNumber = userData[userData.role]["phoneNumber"];
-  const address = userData[userData.role]["address"];
-  const firstName = userData[userData.role]["name"].firstName;
-  const lastName = userData[userData.role]["name"].lastName;
-
   return (
-    <>
-      <section className="block lg:flex justify-between gap-5">
-        <aside className="w-full lg:w-[45%]">
-          {/* profile card */}
-          <div className=" bg-white p-6 rounded-lg shadow-lg">
-            <div className="flex justify-center">
-              <ImageTag
-                url={img}
-                width={100}
-                height={100}
-                style="w-[9rem] h-[9rem] rounded-[50%]"
-                alt="dd"
-              />
-            </div>
-            <div className="mt-4 text-center">
-              <h2 className="text-xl font-bold">
-                Name: {firstName} {lastName}
-              </h2>
-              <h2 className="text-[18px] font-bold text-gray-700">
-                {userData?.role}
-              </h2>
-              <h2 className="text-[18px] font-bold text-gray-700">{address}</h2>
-            </div>
-          </div>
-          {/* website cards */}
-          <div className="bg-white w-full flex flex-col gap-4 shadow-md p-4 py-9 mt-5">
-            {/* Github */}
-            <div className="flex justify-between ">
-              <aside className="flex gap-3">
-                <GithubFilled
-                  style={{ fontSize: "24px", borderRadius: "2px" }}
-                />
-                <h2 className="text-[16px] font-semibold">Github</h2>
-              </aside>
-              <Link href={`https://github.com/sampod76`} target="_blank">
-                https://github.com/sampod76
-              </Link>
-            </div>
-            {/* Facebook */}
-            <div className="flex justify-between ">
-              <aside className="flex gap-3">
-                <FacebookFilled
-                  style={{ fontSize: "24px", borderRadius: "2px" }}
-                />
-                <h2 className="text-[16px] font-semibold">Facebook</h2>
-              </aside>
-              <Link href={`https://facebook.com/sampod76`} target="_blank">
-                https://facebook.com/sampod76
-              </Link>
-            </div>
-            {/* Linkedin */}
-            <div className="flex justify-between ">
-              <aside className="flex gap-3">
-                <LinkedinFilled
-                  style={{ fontSize: "24px", borderRadius: "2px" }}
-                />
-                <h2 className="text-[16px] font-semibold">Linkedin</h2>
-              </aside>
-              <Link href={`https://linkedin.com/sampod76`} target="_blank">
-                https://linkedin.com/sampod76
-              </Link>
-            </div>
-            {/* Twitter */}
-            <div className="flex justify-between ">
-              <aside className="flex gap-3">
-                <TwitterSquareFilled
-                  style={{ fontSize: "24px", borderRadius: "2px" }}
-                />
-                <h2 className="text-[16px] font-semibold">Twitter</h2>
-              </aside>
-              <Link href={`https://twiter.com/sampod76`} target="_blank">
-                https://twiter.com/sampod76
-              </Link>
-            </div>
-          </div>
-        </aside>
-        <aside className="w-full lg:w-[55%] ">
-          {/* Information card */}
-          <div className="flex flex-col gap-3 bg-white shadow-xl p-7 rounded">
-            <div className="flex gap-5 justify-between  text-start  py-3">
-              <h1 className="text-[18px] font-semibold text-start">
-                Full Name
-              </h1>
-              <h3 className="text-[18px] ">
-                {firstName} {lastName}
-              </h3>
-            </div>
-            <div className="flex gap-5 justify-between  text-start  border-t-2 py-3">
-              <h1 className="text-[18px] font-semibold text-start">Email</h1>
-              <h3 className="text-[18px] ">{userData?.role}</h3>
-            </div>
-            <div className="flex gap-5 justify-between  text-start  border-t-2 py-3">
-              <h1 className="text-[18px] font-semibold text-start">
-                Contact No
-              </h1>
-              <h3 className="text-[18px] ">{phoneNumber}</h3>
-            </div>
-            <div className="flex gap-5 justify-between  text-start  border-t-2 py-3">
-              <h1 className="text-[18px] font-semibold text-start">
-                User status
-              </h1>
-              <h3 className="text-[18px] ">{userData?.status}</h3>
-            </div>
-            <div className="flex gap-5 justify-between  text-start  border-t-2 py-3">
-              <h1 className="text-[18px] font-semibold text-start">Address</h1>
-              <h3 className="text-[18px] ">{address}</h3>
-            </div>
-          </div>
-        </aside>
-      </section>
-    </>
+    <div>
+      <ProfilePage userData={userData}></ProfilePage>
+    </div>
   );
 };
 
-export default EditUserPage;
+export default UserDetailsPage;
 
 function useAdminQuery(id: any): { data: any; isLoading: any } {
   throw new Error("Function not implemented.");
--- src/app/(dashboard)/admin/manage-users/students/details/[id]/page.tsx
+++ src/app/(dashboard)/admin/manage-users/students/details/[id]/page.tsx
@@ -1,58 +1,29 @@
 "use client";
 
 import LoadingForDataFetch from "@/components/Utlis/LoadingForDataFetch";
-import { NO_IMAGE } from "@/constants/filePatch";
 
-import { useGetSingleStudentQuery } from "@/redux/api/adminApi/studentApi";
-import { IStudent } from "@/schemas/studentSchema";
-import Image from "next/image";
+import { useGetSingleStudentQuery } from "@/redux/api/adminApi/moderatorApi";
+import StudentProfile from "@/components/profile/StudentProfile";
 
-const EditStudentPage = ({ params }: any) => {
-  // console.log(params, "params");
-  const { data, isLoading: loading } = useGetSingleStudentQuery(params?.id);
-  const studentData:IStudent = data?.data;
+const StudentDetailsPage = ({ params }: any) => {
+  const id = params.id;
+  console.log(id);
+  const { data: userData, isLoading: loading } = useGetSingleStudentQuery(id);
 
-  console.log(studentData);
+  console.log(userData);
 
   if (loading) {
     return <LoadingForDataFetch />;
   }
 
   return (
-    <>
-      <section className="flex justify-center items-center h-screen bg-gray-100">
-        <div className="max-w-md w-full bg-white p-6 rounded-lg shadow-lg">
-          <div className="flex justify-center">
-            <Image
-              width={800}
-              height={800}
-              src={studentData?.img || NO_IMAGE}
-              alt="Profile Image"
-              className="w-60 h-60 rounded-full"
-            />
-          </div>
-          <div className="mt-4 text-center">
-            <h2 className="text-xl font-bold">Name: {studentData?.name?.firstName}</h2>
-            <p className="text-gray-600">Gender: {studentData?.gender}</p>
-            <p className="text-gray-600">
-              Date of Birth:{" "}
-              {new Date(studentData?.dateOfBirth).toLocaleDateString()}
-            </p>
-            <p className="text-gray-600">Phone: {studentData?.phoneNumber}</p>
-            <p className="text-gray-600">Email: {studentData?.email}</p>
-            <p className="text-gray-600">Address: {studentData?.address}</p>
-            <p className="text-gray-600">
-              {" "}
-              Description :{studentData?.address}
-            </p>
-          </div>
-        </div>
-      </section>
-    </>
+    <div>
+      <StudentProfile userData={userData}></StudentProfile>
+    </div>
   );
 };
 
-export default EditStudentPage;
+export default StudentDetailsPage;
 
 function useAdminQuery(id: any): { data: any; isLoading: any } {
   throw new Error("Function not implemented.");
