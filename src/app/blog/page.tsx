33d2c5d8a10564303d52ff74591914111547579e
sampod76 <sampodnath76@gmail.com>
2023-12-27 04:00:39 +0600

    fix texteditor and css problem

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+import parse from 'html-react-parser';
+import TextEditor from '@/components/shared/TextEditor'
+
+import React from 'react'
+
+export default function Blog() {
+  return (
+    <>
+    {/* <TextEditorTiptap/> */}
+    <TextEditor/>
+   
+    </>
+  )
+}
--- src/components/shared/TextEditor.tsx
+++ src/components/shared/TextEditor.tsx
@@ -1,1 +1,34 @@
-jodit-react
+"use client";
+import React, { useState, useRef, useMemo } from "react";
+import JoditEditor from "jodit-react";
+import parse from "html-react-parser";
+const TextEditor = (/* {setValue} */) => {
+  const editor = useRef(null);
+  const [content, setContent] = useState("");
+  const [vlaue, setValue] = useState("");
+  console.log("🚀 ~ file: textEditor.js:8 ~ TextEditor ~ vlaue:", vlaue);
+
+  const editorConfig = useMemo(
+    () => ({
+      readonly: false,
+      placeholder: "Start typing...",
+    }),
+    []
+  );
+
+  return (
+    <div>
+      <JoditEditor
+        ref={editor}
+        config={editorConfig}
+        value={content}
+        // tabIndex={1} // tabIndex of textarea
+        onBlur={(newContent) => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
+        onChange={(newContent) => setValue(newContent)}
+      />
+      <div className="my-5 jodit-wysiwyg">{parse(vlaue)}</div>
+    </div>
+  );
+};
+
+export default TextEditor;
