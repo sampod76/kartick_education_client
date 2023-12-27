12aa5012bb24fb88ebce83c6a9017bde3a35c018
sarwar-asik <sarwarasik@gmail.com>
2023-12-27 15:49:02 +0600

    module_list

--- src/app/(public)/module/[id]/page.tsx
+++ src/app/(public)/module/[id]/page.tsx
@@ -1,15 +1,17 @@
+import ModuleList from "@/components/module/ModuleList";
 import ModuleTab from "@/components/module/ModuleTab";
 import ModuleTop from "@/components/module/ModuleTop";
 import React from "react";
 
 export default function ModulePage({ params }: { params: { id: string } }) {
-    const milestoneId = params?.id
+  const milestoneId = params?.id;
   console.log(milestoneId);
   return (
     <div>
       <h1>ModulePageDetails Page</h1>
-      <ModuleTop milestoneId={milestoneId} />
-      <ModuleTab milestoneId={milestoneId}/>
+      <ModuleList milestoneId={milestoneId}></ModuleList>
+      {/* <ModuleTop milestoneId={milestoneId} />
+      <ModuleTab milestoneId={milestoneId}/> */}
     </div>
   );
 }
