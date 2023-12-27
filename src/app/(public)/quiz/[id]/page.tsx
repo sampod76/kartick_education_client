43a9d1df0a8036672d9cf13ccb127ce34afe5150
sarwar-asik <sarwarasik@gmail.com>
2023-12-27 22:47:36 +0600

    quize_page_static

--- src/app/(public)/quiz/[id]/page.tsx
+++ src/app/(public)/quiz/[id]/page.tsx
@@ -1,10 +1,29 @@
 import Quizes from "@/components/Quiz/Quizes";
+import UMBreadCrumb from "@/components/ui/UMBreadCrumb";
 import React from "react";
 
 export default function QuizPage() {
   return (
     <div className=" w-full lg:w-[80vw] mx-auto">
-      <h1 className="text-3xl font-bold">Be Smart Part Quiz</h1>
+      <UMBreadCrumb
+        items={[
+          {
+            label: "Leadership",
+            link: "/course",
+          },
+          {
+            label: "Public Lead",
+            link: "/course/milstone",
+          },
+          {
+            label: "Be Smart Lesson Quiz",
+            link: "/course/module",
+          },
+        ]}
+      />
+      <h1 className="text-3xl font-bold my-9 text-slate-700">
+        Be Smart Lesson Quiz
+      </h1>
       <Quizes />
     </div>
   );
