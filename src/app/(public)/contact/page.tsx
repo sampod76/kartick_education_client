dd01f4350b36571faae158062c886b64201f2057
sarwar-asik <sarwarasik@gmail.com>
2024-01-04 22:53:36 +0600

    contact-done

---
+++ src/app/(public)/contact/page.tsx
@@ -0,0 +1,10 @@
+import ContactPage from "@/components/contact/ContactPage";
+import React from "react";
+
+export default function Contact() {
+  return (
+    <div>
+      <ContactPage />
+    </div>
+  );
+}
