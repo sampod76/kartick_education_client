3e5b19b3b172f17fc2963516ba48fdf0222041d3
sampod76 <sampodnath76@gmail.com>
2024-01-11 23:20:52 +0600

    adddsdd

---
+++ src/app/(dashboard)/trainers/category/create/course/[categoryId]/page.tsx
@@ -0,0 +1,9 @@
+import CreateCourseByCategory from '@/components/Course/CreateCourseByCategory'
+import React from 'react'
+
+export default function CreateAdminCourseByCategoryPage() {
+  return (
+    <div><CreateCourseByCategory/></div>
+  )
+}
+
