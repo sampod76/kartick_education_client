6d42aac94402c59776f59cbceae439fdec8c60d2
sarwar-asik <sarwarasik@gmail.com>
2024-01-13 17:46:07 +0600

    package_card_designed

--- src/components/subscription/join/JoinMain.tsx
+++ src/components/subscription/join/JoinMain.tsx
@@ -1,19 +1,21 @@
 "use client";
 import React, { useState } from "react";
 import JoinSelect from "./JoinSelect";
+import JoinPackage from "./JoinPackage";
 
 export default function JoinMain() {
   const [plan, setPlan] = useState<string>("Monthly");
   const [quantity, setQuantity] = useState<number>(1);
 
   return (
-    <div className="w-full lg:max-w-4xl mx-auto p-3">
+    <div className="w-full lg:max-w-7xl mx-auto p-3">
       <JoinSelect
         plan={plan}
         setPlan={setPlan}
         quantity={quantity}
         setQuantity={setQuantity}
       />
+      <JoinPackage />
     </div>
   );
 }
