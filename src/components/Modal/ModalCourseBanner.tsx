234557e543ab6e8c422305e4dc17956151858403
sarwar-asik <sarwarasik@gmail.com>
2024-01-17 16:26:30 +0600

    modal_added

---
+++ src/components/Modal/ModalCourseBanner.tsx
@@ -0,0 +1,13 @@
+"use clint";
+import React from "react";
+
+export default function ModalCourseBanner({
+  categoryId,
+  setIsModalOpen,
+}: {
+  categoryId: string;
+  setIsModalOpen: any;
+}) {
+  console.log(categoryId);
+  return <div>ModalCourseBanner</div>;
+}
