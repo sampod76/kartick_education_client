a3415d695dcaa27b9a2559f5837319b1c67dd04b
sarwar-asik <sarwarasik@gmail.com>
2024-01-19 00:17:47 +0600

    user_profile_updated

--- src/components/package/SinglePurchasedCar.tsx
+++ src/components/package/SinglePurchasedCar.tsx
@@ -15,8 +15,8 @@ export default function SInglePurchased({
     <div
     // href={`/packages/milestone/${packages?._id}?category=${packages?.category?._id}`}
     >
-      <div className="py-2">
-        <div className="flex max-w-md bg-white shadow-lg rounded-lg overflow-hidden">
+      <div className="p-2">
+        <div className="flex w-full lg:max-w-3xl bg-white shadow-lg rounded-lg overflow-hidden">
           <div
             className="w-1/3 bg-cover"
             style={{
@@ -39,38 +39,12 @@ export default function SInglePurchased({
                 {packages?.membership?.title}
               </EllipsisMiddle>
             </p>
-            <div className="flex item-center mt-2">
-              <svg
-                className="w-5 h-5 fill-current text-yellow-700"
-                viewBox="0 0 24 24"
-              >
-                <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
-              </svg>
-              <svg
-                className="w-5 h-5 fill-current text-yellow-700"
-                viewBox="0 0 24 24"
-              >
-                <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
-              </svg>
-              <svg
-                className="w-5 h-5 fill-current text-yellow-700"
-                viewBox="0 0 24 24"
-              >
-                <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
-              </svg>
-              <svg
-                className="w-5 h-5 fill-current text-yellow-500"
-                viewBox="0 0 24 24"
-              >
-                <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
-              </svg>
-              <svg
-                className="w-5 h-5 fill-current text-yellow-500"
-                viewBox="0 0 24 24"
-              >
-                <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
-              </svg>
+            <div className="mt-2 flex justify-between ">
+              <h1 className ="text-sm text-gray">Total Student: {packages?.total_purchase_student}</h1>
+              <h1 className ="text-sm text-gray">Status: {packages?.paymentStatus}</h1>
+              
             </div>
+           
             <div className="flex item-center justify-between mt-3">
               <h1 className="text-gray-700 font-bold text-xl">
                 {packages?.purchase?.price} /{packages?.purchase?.label}
