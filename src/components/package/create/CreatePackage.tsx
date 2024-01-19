edd226fe7a7e694f425b12d0dad51a2dce420375
sampod76 <sampodnath76@gmail.com>
2024-01-19 18:16:06 +0600

    add pakde bysb

--- src/components/package/create/CreatePackage.tsx
+++ src/components/package/create/CreatePackage.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
-import { Button, Form, Input, Space ,  InputNumber} from "antd";
+import { Button, Form, Input, Space, InputNumber } from "antd";
 import { useGetAllCategoryQuery } from "@/redux/api/adminApi/categoryApi";
 import { ENUM_STATUS, ENUM_YN } from "@/constants/globalEnums";
 import { Select } from "antd";
@@ -14,15 +14,14 @@ import { Error_model_hook, Success_model } from "@/utils/modalHook";
 
 // ! for uuid
 const generateUUID = () => {
-  return 'xxxxxxxx-xxxx-4xxx'.replace(/[xy]/g, function (c) {
+  return "xxxxxxxx-xxxx-4xxx".replace(/[xy]/g, function (c) {
     const r = (Math.random() * 16) | 0;
-    const v = c === 'x' ? r : (r & 0x3) | 0x8;
+    const v = c === "x" ? r : (r & 0x3) | 0x8;
     return v.toString(16);
   });
 };
 export default function CreatePackage() {
-
-    const [form] = Form.useForm();
+  const [form] = Form.useForm();
 
   const uuid = generateUUID();
   // console.log(uuid,"uuiduuid")
@@ -37,7 +36,8 @@ export default function CreatePackage() {
     value: select._id,
   }));
 
-  const [addPackage,{isSuccess}] = useAddPackageMutation();
+  const [addPackage, { isSuccess, isLoading: AddPackageLoading }] =
+    useAddPackageMutation();
 
   const onChange = (value: string) => {
     console.log(`selected ${value}`);
@@ -60,8 +60,8 @@ export default function CreatePackage() {
     console.log("Received values of form:", values);
     const packageData = {
       membership: {
-        title:values.membership?.title,
-        uid:uuid
+        title: values.membership?.title,
+        uid: uuid,
       },
       title: values.title,
       type: values.type,
@@ -79,7 +79,7 @@ export default function CreatePackage() {
         Error_model_hook(res?.message);
       } else {
         Success_model("Successfully added Package");
-        form.resetFields()
+        form.resetFields();
       }
       // console.log(res);
     } catch (error: any) {
@@ -89,126 +89,124 @@ export default function CreatePackage() {
   };
   return (
     <div className="bg-white shadow-lg p-5 rounded-xl">
-        <h1 className="text-xl font-bold border-b-2 border-spacing-4 mb-2  ">
-       Create Package
-        </h1>
+      <h1 className="text-xl font-bold border-b-2 border-spacing-4 mb-2  ">
+        Create Package
+      </h1>
       <Form
         name="package_create"
         onFinish={onFinish}
         form={form}
-        style={{ maxWidth: 850 ,marginInline:"auto",border:"0.2px solid gray",padding:"8px",borderRadius:"5px"}}
+        style={{
+          maxWidth: 850,
+          marginInline: "auto",
+          border: "0.2px solid gray",
+          padding: "8px",
+          borderRadius: "5px",
+        }}
         autoComplete="off"
         layout="vertical"
-        
       >
         <Form.Item>
           <Form.Item name="title" label="Title">
             <Input size="large" placeholder="Please enter package title" />
           </Form.Item>
-          <Space
-             
-            >
-          <Form.Item name="type" label="Select Types">
-            {/* <LabelUi>Select Types </LabelUi> */}
-            <Select
-              style={{ maxWidth: "100%" }}
-              placeholder="Select Types"
-              size="large"
-            >
-              <Option value="bundle">Bundle</Option>
-              <Option value="select">Select</Option>
-              <Option value="multiple_select">Multiple Select</Option>
-            </Select>
-          </Form.Item>
-          <Form.Item name={["membership", "title"]} label="Select Membership">
-            {/* <LabelUi>Select Membership </LabelUi> */}
-            <Select
-              style={{ width: "100%" }}
-              placeholder="Select Membership"
-              size="large"
-            >
-              <Option value="family-personal">Family & Personal</Option>
-              <Option value="school-teacher">School & Teacher</Option>
-            </Select>
-          </Form.Item>
-
-        
-          
-     
-            </Space>
+          <Space>
+            <Form.Item name="type" label="Select Types">
+              {/* <LabelUi>Select Types </LabelUi> */}
+              <Select
+                style={{ maxWidth: "100%" }}
+                placeholder="Select Types"
+                size="large"
+              >
+                <Option value="bundle">Bundle</Option>
+                <Option value="select">Select</Option>
+                <Option value="multiple_select">Multiple Select</Option>
+              </Select>
+            </Form.Item>
+            <Form.Item name={["membership", "title"]} label="Select Membership">
+              {/* <LabelUi>Select Membership </LabelUi> */}
+              <Select
+                style={{ width: "100%" }}
+                placeholder="Select Membership"
+                size="large"
+              >
+                <Option value="family & personal">Family & Personal</Option>
+                <Option value="school & teacher">School & Teacher</Option>
+              </Select>
+            </Form.Item>
+          </Space>
           <div className="">
-          
-                {/*//!  monthly */}
-                <Space.Compact>
-                <Form.Item
-                  name={["monthly", "price"]}
-                  // noStyle
-                  
-                  label="Monthly Price"
-                  rules={[{ required: true, message: "Province is required" }]}
-                >
-                  <InputNumber
-                    name="price"
-                    type="number"
-                    placeholder="Monthly Price"
-                    // style={{ width: "70%" }}
-                  />
-                </Form.Item>
-
-                <Form.Item
-                  name={["monthly", "each_student_increment"]}
-                  // noStyle
-                  label="Each Student price"
-                  rules={[
-                    {
-                      required: true,
-                      message: "Each Student Price is required",
-                    },
-                  ]}
-                >
-                  <InputNumber
-                    style={{ width: "70%" }}
-                    type="number"
-                    placeholder="Input Each Student Price"
-                  />
-                </Form.Item>
-              </Space.Compact>
+            {/*//!  monthly */}
             <Space.Compact>
-                   {/*//!  biannual */}
-                <Form.Item
-                  name={["biannual", "price"]}
-                  // noStyle
-                  label="Biannual Price"
-                  rules={[{ required: true, message: "Province is required" }]}
-                >
-                  <InputNumber
-                    name="price"
-                    type="number"
-                    placeholder="Biannual Price"
-                    // style={{ width: "70%" }}
-                  />
-                </Form.Item>
+              <Form.Item
+                name={["monthly", "price"]}
+                // noStyle
 
-                <Form.Item
-                  name={["biannual", "each_student_increment"]}
-                  // noStyle
-                  label="Each Student price"
-                  rules={[
-                    {
-                      required: true,
-                      message: "Each Student Price is required",
-                    },
-                  ]}
-                >
-                  <InputNumber
-                    style={{ width: "70%" }}
-                    type="number"
-                    placeholder="Input Each Student Price"
-                  />
-                </Form.Item>
-              </Space.Compact>
+                label="Monthly Price"
+                rules={[{ required: true, message: "Province is required" }]}
+              >
+                <InputNumber
+                  name="price"
+                  type="number"
+                  placeholder="Monthly Price"
+                  // style={{ width: "70%" }}
+                />
+              </Form.Item>
+
+              <Form.Item
+                name={["monthly", "each_student_increment"]}
+                // noStyle
+                label="Each Student price"
+                rules={[
+                  {
+                    required: true,
+                    message: "Each Student Price is required",
+                  },
+                ]}
+              >
+                <InputNumber
+                  style={{ width: "70%" }}
+                  type="number"
+                  placeholder="Input Each Student Price"
+                />
+              </Form.Item>
+            </Space.Compact>
             <Space.Compact>
-                       {/*//!  yearly */}
+              {/*//!  biannual */}
+              <Form.Item
+                name={["biannual", "price"]}
+                // noStyle
+                label="Biannual Price"
+                rules={[{ required: true, message: "Province is required" }]}
+              >
+                <InputNumber
+                  name="price"
+                  type="number"
+                  placeholder="Biannual Price"
+                  // style={{ width: "70%" }}
+                />
+              </Form.Item>
+
+              <Form.Item
+                name={["biannual", "each_student_increment"]}
+                // noStyle
+                label="Each Student price"
+                rules={[
+                  {
+                    required: true,
+                    message: "Each Student Price is required",
+                  },
+                ]}
+              >
+                <InputNumber
+                  style={{ width: "70%" }}
+                  type="number"
+                  placeholder="Input Each Student Price"
+                />
+              </Form.Item>
+            </Space.Compact>
+            <Space.Compact>
+              {/*//!  yearly */}
               <Form.Item
                 name={["yearly", "price"]}
                 // noStyle
@@ -244,63 +242,70 @@ export default function CreatePackage() {
           </div>
         </Form.Item>
         <div className="border-2 rounded-lg p-3">
-      <LabelUi>Add Category</LabelUi>
+          <LabelUi>Add Category</LabelUi>
           <Form.List name="categories">
             {(fields, { add, remove }) => {
-    // console.log(fields,'fieldsfieldsfieldsfields') ;
+              // console.log(fields,'fieldsfieldsfieldsfields') ;
 
-    const onchange=(value:any)=>{
-      // console.log(value,'value') ;
-        const updatedOptions = options?.filter((item) => item?.value !== value);
-      // console.log(updatedOptions)
-      options = updatedOptions;
-      // console.log(options)
-    }
+              const onchange = (value: any) => {
+                // console.log(value,'value') ;
+                const updatedOptions = options?.filter(
+                  (item) => item?.value !== value
+                );
+                // console.log(updatedOptions)
+                options = updatedOptions;
+                // console.log(options)
+              };
               return (
                 <>
-                  {fields.map(({ key, name, ...restField }) => (                    <Space key={key} style={{
-    display: "flex",
-    // flexDirection: "column", // Stack items vertically on smaller screens
-    margin: "8px auto",
-    // background: "blue",
-    width: "100%",
-  }}
-  align="center"
->
-  <Form.Item
-    {...restField}
-    style={{ width: "", marginBottom: "8px" }}
-    name={[name, "category"]}
-    rules={[
-      { required: true, message: "Missing Category" },
-    ]}
-  >
-    <Select
-      onChange={onchange}
-      loading={isLoading}
-      style={{ width: "" }}
-      placeholder="Select category"
-      size="large"
-      options={options}
-    />
-  </Form.Item>
-  <Form.Item
-    {...restField}
-    name={[name, "label"]}
-    style={{ width: "", marginBottom: "8px", maxWidth: "200px" }}
-    rules={[{ required: true, message: "Missing Category Label" }]}
-  >
-    <Input
-      size="large"
-      placeholder="label"
-    />
-  </Form.Item>
-  <MinusCircleOutlined
-    onClick={() => remove(name)}
-    style={{ marginInline: "3px" }}
-  />
-</Space>
-
+                  {fields.map(({ key, name, ...restField }) => (
+                    <Space
+                      key={key}
+                      style={{
+                        display: "flex",
+                        // flexDirection: "column", // Stack items vertically on smaller screens
+                        margin: "8px auto",
+                        // background: "blue",
+                        width: "100%",
+                      }}
+                      align="center"
+                    >
+                      <Form.Item
+                        {...restField}
+                        style={{ width: "", marginBottom: "8px" }}
+                        name={[name, "category"]}
+                        rules={[
+                          { required: true, message: "Missing Category" },
+                        ]}
+                      >
+                        <Select
+                          onChange={onchange}
+                          loading={isLoading}
+                          style={{ width: "" }}
+                          placeholder="Select category"
+                          size="large"
+                          options={options}
+                        />
+                      </Form.Item>
+                      <Form.Item
+                        {...restField}
+                        name={[name, "label"]}
+                        style={{
+                          width: "",
+                          marginBottom: "8px",
+                          maxWidth: "200px",
+                        }}
+                        rules={[
+                          { required: true, message: "Missing Category Label" },
+                        ]}
+                      >
+                        <Input size="large" placeholder="label" />
+                      </Form.Item>
+                      <MinusCircleOutlined
+                        onClick={() => remove(name)}
+                        style={{ marginInline: "3px" }}
+                      />
+                    </Space>
                   ))}
                   <Form.Item>
                     <Button
@@ -313,13 +318,17 @@ export default function CreatePackage() {
                     </Button>
                   </Form.Item>
                 </>
-              )
+              );
             }}
           </Form.List>
         </div>
         <Form.Item>
           <div className="flex justify-center items-center mt-3">
-            <Button loading={isSuccess} type="default" htmlType="submit">
+            <Button
+              loading={AddPackageLoading}
+              type="default"
+              htmlType="submit"
+            >
               Create
             </Button>
           </div>
