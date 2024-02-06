52e6a0bef41d437e350e77e50ae0927a62222f6f
sarwar-asik <sarwarasik@gmail.com>
2024-02-06 23:59:55 +0600

    advance_classList

--- src/app/(dashboard)/admin/features/advance-class/page.tsx
+++ src/app/(dashboard)/admin/features/advance-class/page.tsx
@@ -1,7 +1,8 @@
+import AdvanceClassList from '@/components/features/advance-class/AdvanceClassDashList.tsx'
 import React from 'react'
 
 export default function AdvanceClassPage() {
   return (
-    <div>AdvanceClassPage</div>
+    <div><AdvanceClassList /></div>
   )
 }
