eafdfcd55d8234e5fc7c17537cc4dd7a3fd590dd
sarwar-asik <sarwarasik@gmail.com>
2024-02-08 00:32:33 +0600

    tab_created

--- src/components/analytics/AnalyticsTab.tsx
+++ src/components/analytics/AnalyticsTab.tsx
@@ -1,21 +1,67 @@
+'use client'
 import React from 'react';
-import { AndroidOutlined, AppleOutlined } from '@ant-design/icons';
-import { Tabs } from 'antd';
+import { AndroidOutlined, AppleOutlined, UserSwitchOutlined, DiffOutlined, PicRightOutlined } from '@ant-design/icons';
+import { Tabs, TabsProps } from 'antd';
 
 export default function AnalyticsTab() {
+
+  const tabsItems: TabsProps["items"] = [
+    {
+      label: (
+        <button className="text-lg lg:text-xl text-center font-bold    px-3 py-2 flex gap-2 border-r-2 border-slate-600">
+          <UserSwitchOutlined
+            style={{
+              fontSize: "1.5rem",
+            }}
+          />{" "}
+          <h1 className=" text-sm md:text-lg lg:text-2xl">Usage</h1>
+        </button>
+      ),
+      key: "1",
+      children: 'Usage section'
+    },
+    {
+      label: (
+        <button className="text-lg lg:text-xl text-center font-bold    px-3 py-2 flex gap-2 border-r-2 border-slate-600">
+          <DiffOutlined
+            style={{
+              fontSize: "1.5rem",
+            }}
+          />{" "}
+          <h1 className=" text-sm md:text-lg lg:text-2xl">Scores Section</h1>
+        </button>
+      ),
+      key: "Scores",
+      children: 'Scores'
+    },
+    {
+      label: (
+        <button className="text-lg lg:text-xl text-center font-bold    px-3 py-2 flex gap-2 border-r- border-slate-600">
+          <PicRightOutlined
+            style={{
+              fontSize: "1.5rem",
+            }}
+          />{" "}
+          <h1 className=" text-sm md:text-lg lg:text-2xl">Progress Section</h1>
+        </button>
+      ),
+      key: "Progress",
+      children: 'Progress'
+    },
+
+  ]
   return (
-    <div>
+    <div className='w-full lg:max-w-[70%] mx-auto'>
       <Tabs
         defaultActiveKey="2"
-        items={[AppleOutlined, AndroidOutlined].map((Icon, i) => {
-          const id = String(i + 1);
-          return {
-            key: id,
-            label: `Tab ${id}`,
-            children: `Tab ${id}`,
-            icon: <Icon />,
-          };
-        })}
+        items={tabsItems}
+        indicatorSize={100}
+        style={{
+
+        }}
+      // tabPosition="bottom"
+
+      // indicator={{ size: (origin) => origin - 20, align: 'center' }}
       />
 
     </div>
---
+++ src/components/analytics/progress/ProgressAnalytics.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ProgressAnalytics() {
+  return (
+    <div>ProgressAnalytics</div>
+  )
+}
