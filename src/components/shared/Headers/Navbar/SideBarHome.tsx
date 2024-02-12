788fa086db5a7bafd59cb79d4c2d4cb8cba4d351
sarwar-asik <sarwarasik@gmail.com>
2024-02-13 00:38:53 +0600

    fixed_navs

--- src/components/shared/Headers/Navbar/SideBarHome.tsx
+++ src/components/shared/Headers/Navbar/SideBarHome.tsx
@@ -6,6 +6,8 @@ import { MenuUnfoldOutlined, CloseOutlined } from "@ant-design/icons";
 import Logo from "../../Logo";
 import MenuUI from "@/components/ui/NavUI/MenuUI";
 import homeSIdeItems from "@/constants/homeSideBarItems";
+import Link from "next/link";
+import UserAvatarUI from "@/components/ui/NavUI/UserAvatarUI";
 
 const SideBarHome = ({ userInfo }: { userInfo: any }) => {
   const [open, setOpen] = useState(false);
@@ -22,7 +24,24 @@ const SideBarHome = ({ userInfo }: { userInfo: any }) => {
   const sideItemsHome = homeSIdeItems(userInfo?.role ? userInfo.role : null)
   return (
     <>
-      <Space className="text-2xl ">
+      <Space className="text-2xl flex text-gray-200">
+        {
+          userInfo?.email ? (
+            <UserAvatarUI />
+          ) : (
+            <div className="flex font-[700] max-h-[2.7rem] lg:max-h-[3.3rem]">
+              <Link href="/login">
+                <a className="cursor-pointer font-semibold overflow-hidden relative z-100 border border-[#5F8122] group px-5 py-2 bg-white rounded-[36px] uppercase">
+                  <span className="relative z-10 text-[#5F8122] group-hover:text-white text-lg duration-500">
+                    Login
+                  </span>
+                  <span className="absolute w-full h-full bg-[#5F8122] -left- top-0 -rotate-45 group-hover:rotate-0 group-hover:left-0 duration-500"></span>
+                  <span className="absolute w-full h-full bg-[#5F8122] -right- top-0 -rotate-45 group-hover:rotate-0 group-hover:right-0 duration-500"></span>
+                </a>
+              </Link>
+            </div>
+          )
+        }
         {!open ? (
           <MenuUnfoldOutlined onClick={showDrawer} />
         ) : (
