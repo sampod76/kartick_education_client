dc5139089c84ea1863534649f120d79dc1eda35c
sarwar-asik <sarwarasik@gmail.com>
2024-02-12 15:05:06 +0600

    milestonList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,23 +13,27 @@ import React from "react";
 import { EllipsisMiddle } from "@/utils/CutTextElliples";
 import ShortOverViewHomePage from "@/components/Home/ShortOverView/ShortOverViewHome";
 import AdvanceHomeClass from "@/components/Home/AdvanceClass/AdvanceClassHome";
+import MilestoneHomeFeatures from "@/components/Home/Milestonehome/MilestoneHomeFeatures";
+import MilestoneHomeFeatures2 from "../components/Home/Milestonehome/MilestoneFetures2";
 
 const HomePage = () => {
   return (
     <div className="">
       <HomeHeader />
-      
-      <div className="-mt-[6rem] bg-[#BEDDF9]">
+
+      <div className="-mt-[6rem] bg-[#BEDDF9">
         <div className="w-full min-h-[5rem]"></div>
         <BannerSection />
       </div>
-      
-      <main className="bg-[#A2B0F321] pb-10 min-h-screen">
+
+      <main className="bg-[#A2B0F321 pb-10 min-h-screen">
         <CartDrawer />
-        <CommonCourse />
+        {/* <CommonCourse /> */}
         <CoursesTab />
         <SupportHero />
-        <ShortOverViewHomePage />
+        {/* <ShortOverViewHomePage /> */}
+        <MilestoneHomeFeatures />
+        <MilestoneHomeFeatures2 />
         <TestimonialMain />
         {/* <Programmes /> */}
         <AdvanceHomeClass />
