e82607e531d06b238217d1ed93709f444bd6f417
sarwar-asik <sarwarasik@gmail.com>
2024-02-14 18:59:12 +0600

    bannerMembarshipt

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,3 +1,4 @@
+import BannerSignUp from "@/components/Home/Banner&hero/BannerSignup";
 import Login from "@/components/Login/LoginPage";
 import LoginStudent from "@/components/Login/LoginStudent";
 import { Metadata } from "next";
@@ -8,6 +9,10 @@ export const metadata: Metadata = {
 const LoginPage = () => {
   return (
     <>
+      {/* <div className="-mt-[2rem] mb-4 lg:mb-6 ">
+        <div className="w-full min-h-[3.3rem] bg-[#BEDDF9]"></div>
+        <BannerSignUp />
+      </div> */}
       {/* <LoginStudent/>*/}
       <Login />
     </>
