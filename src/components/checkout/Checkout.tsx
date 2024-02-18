a631e783c9965b3d4bde9d957c7374fe07a29857
sarwar-asik <sarwarasik@gmail.com>
2024-02-18 23:56:02 +0600

    checkout=>done

--- src/components/checkout/Checkout.tsx
+++ src/components/checkout/Checkout.tsx
@@ -5,6 +5,7 @@ import { IDecodedInfo, getUserInfo } from '@/services/auth.service';
 import { Row, Space, Spin } from 'antd';
 import Image from 'next/image';
 import { AllImage } from '@/assets/AllImge';
+import { LockOutlined } from "@ant-design/icons"
 
 export default function Checkout() {
     const userInfo = getUserInfo() as IDecodedInfo;
@@ -35,11 +36,11 @@ export default function Checkout() {
         );
     }
     return (
-        <div className='min-h-screen'>
+        <div className='min-h-screen px-2'>
 
-            <div className="w-full lg:w-[86%] mx-auto my-9 flex items-center gap-3 text-lg">
+            <div className="container mx-auto my-9 flex items-center gap-3 text-lg">
                 <UserAvatarUI />
-                <h5>Logged in as {userInfo?.email} </h5>
+                <h5 className='text-lg lg:text-xl'>Logged in as {userInfo?.email} </h5>
             </div>
             <div className="container mx-auto">
                 <h1 className='text-lg md:text-xl lg:text-2xl '>Your Order</h1>
@@ -63,11 +64,11 @@ export default function Checkout() {
                         <tbody>
                             <tr className="border">
                                 <td className="py-5 px-4 border text-[#797979]">Sub Total</td>
-                                <td className="py-5 px-4 border">$10.00</td>
+                                <td className="py-5 px-4 border text-xl lg:text-2xl font-bold">$ 10.00</td>
                             </tr>
                             <tr className="bg-gray-100 border">
                                 <td className="py-5 px-4 border text-[#797979]">Total</td>
-                                <td className="py-5 px-4 border">$15.00</td>
+                                <td className="py-5 px-4 border text-xl lg:text-2xl font-bold">$ 15.00</td>
                             </tr>
 
                         </tbody>
@@ -76,10 +77,21 @@ export default function Checkout() {
             </div>
 
             <div className="mt-5 container mx-auto">
-                <h2 className='my-3'>Additional Note </h2>
-                <textarea className='w-full min-h-[7rem] border border-gray-700' />
+                <h2 className='my-3 text-lg lg:text-xl'>Additional Note </h2>
+                <textarea className='w-full min-h-[4rem] lg:min-h-[8rem] border border-[#DADADA] outline-none p-3' />
 
-                <h2 className='text-xl font-bold uppercase my-3'>PAYMENT</h2>
+                <div className="mt-7">
+                    <div className="flex justify-between items-center uppercase">
+                        <h2 className='text-lg lg:text-xl font-bold uppercase my-3'>PAYMENT</h2>
+                        <p className='text-sm text-slate-600'>
+                            <LockOutlined style={{ marginInline: "3px" }} />
+                            secure payment</p>
+                    </div>
+
+                    <h6>Paypal button</h6>
+
+                    <button className='bg-[#5371FF] h- p-3 mt-5 text-lg lg:text-xl font-bold text-white rounded uppercase'>Place Order</button>
+                </div>
             </div>
         </div>
     )
