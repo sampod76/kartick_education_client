fce96463da82f9885af23de9473b394cbcf5f9c4
sarwar-asik <sarwarasik@gmail.com>
2024-02-21 14:07:56 +0600

    solve_milestoneView

--- src/components/milestone/details/DetailsMilestoneDash.tsx
+++ src/components/milestone/details/DetailsMilestoneDash.tsx
@@ -8,11 +8,13 @@ import { IMilestoneData } from '@/types/miestoneType';
 import { CiClock2 } from 'react-icons/ci';
 import { EllipsisMiddle } from '@/utils/CutTextElliples';
 import { AllImage } from '@/assets/AllImge';
-
+import notFoundImga from "@/assets/noImge.jpg"
 export default function DetailsMilestoneDash({ milestoneId }: { milestoneId: string }) {
 
 
-
+    const { data: milestoneSingleData } = useGetSingleMilestoneQuery(milestoneId)
+    console.log(milestoneSingleData, 'milestoneSingleData')
+    // console.log(milestoneId,'milestoneId')
     const query: Record<string, any> = {};
     //! for Course options selection
     query["limit"] = 999999;
@@ -21,7 +23,8 @@ export default function DetailsMilestoneDash({ milestoneId }: { milestoneId: str
 
     //! for Module options selection
     const { data: moduleData, isLoading } = useGetAllModuleQuery({ ...query, milestone: milestoneId });
-    // console.log(moduleData, 'moduleData')
+    // console.log(moduleData, 'moduleData',milestoneId)
+
     if (isLoading) {
         return (<div className="">
             <LoadingSkeleton number={20} />
@@ -31,8 +34,8 @@ export default function DetailsMilestoneDash({ milestoneId }: { milestoneId: str
     const milestoneData = { ...moduleData?.data[0]?.milestone }
 
 
-    const singleMilestoneData: IMilestoneData = {
-        ...milestoneData,
+    const singleMilestoneFullData: IMilestoneData = {
+        ...milestoneSingleData,
         modules: moduleData?.data
     }
 
@@ -44,46 +47,45 @@ export default function DetailsMilestoneDash({ milestoneId }: { milestoneId: str
             const v = c === 'x' ? r : (r & 0x3) | 0x8;
             return v.toString(16); // Convert each character to a string
         });
-
         // Convert the hexadecimal string to a decimal number
         const uuidNumber = parseInt(uuidString.replace(/[^0-9a-f]/gi, ''), 16);
-
         return uuidNumber;
     };
 
 
+    // console.log(milestoneData, 'milestoneData')
+    // console.log(singleMilestoneFullData)
+    // const ImgUrl =milestoneSingleData?.imgs?.length ? milestoneSingleData?.imgs[0] : AllImage?.notFoundImage
+    // console.log(ImgUrl,'ImgUrl')
 
-    // console.log(singleMilestoneData)
+
     return (
         <div>
             <h2 className='text-center text-xl my-3'>Milestone Details </h2>
 
             <div className="block lg:flex gap-2">
 
-
+                {/* backgroundImage: `url(${milestoneData?.imgs?.length ? milestoneData?.imgs[0] : AllImage?.notFoundImage})`, */}
                 <div className="p-2 border-2 shadow-md  rounded-xl flex max-w-xl bg-white  overflow-hidden">
                     <div className="w-1/3 bg-cover" style={{
-                        backgroundImage: `url(${milestoneData?.imgs[0] || AllImage?.notFoundImage})`
+                        backgroundImage: `url(${milestoneSingleData?.imgs?.length ? milestoneSingleData?.imgs[0] : AllImage?.notFoundImage})`
                     }}>
                     </div>
                     <div className="w-2/3 p-2 ">
                         <h1 className="text-gray-900 font-bold text-2xl uppercase">  <EllipsisMiddle suffixCount={3} maxLength={90}>
-                            {milestoneData?.title}
+                            {milestoneSingleData?.title}
                         </EllipsisMiddle></h1>
                         <p className="mt-2 text-gray-600 text-sm">  <EllipsisMiddle suffixCount={3} maxLength={160}>
-                            {milestoneData?.short_description}
+                            {milestoneSingleData?.short_description}
                         </EllipsisMiddle></p>
 
                         <div className="flex item-center justify-between mt-3">
-                            <h1 className="text-gray-700 font-bold text-xl">Milestone No : {milestoneData?.milestone_number}</h1>
-
+                            <h1 className="text-gray-700 font-bold text-xl">Milestone No : {milestoneSingleData?.milestone_number}</h1>
                         </div>
                     </div>
                 </div>
-
-
                 <div className="max-w-xl">
-                    <SingleMilestone milestoneData={singleMilestoneData} index={generateUUID()} />
+                    <SingleMilestone milestoneData={singleMilestoneFullData} index={generateUUID()} />
                 </div>
             </div>
         </div>
