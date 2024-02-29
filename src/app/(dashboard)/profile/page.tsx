556a19d3dfc019eb45541e681cec0dbe0aa47b4a
sampod76 <sampodnath76@gmail.com>
2024-02-29 20:01:42 +0600

    add fix profle

--- src/app/(dashboard)/profile/page.tsx
+++ src/app/(dashboard)/profile/page.tsx
@@ -32,64 +32,7 @@ const ProfileTemplate = () => {
 
   return (
     <div>
-      <div className="flex justify-end items-end">
-        {/* <Button onClick={() => setUpdate(!update)}   type="default">
-          {update ? "profile" : "Update/edit profile"}
-        </Button> */}
-      </div>
-      {/* {update ? (
-        <UpdateProfile></UpdateProfile>
-      ) : (
-        <div className="flex justify-center items-center min-h-screen">
-          <div className=" border-2 rounded-2xl shadow-2xl w-fit p-5">
-            <div>
-              <Title level={2}>Profile</Title>
-              <Space align="center" direction="horizontal">
-                <Row gutter={16}>
-                  <Col span={8}>
-                    <Image
-                      width={800}
-                      height={800}
-                      src={userData?.profileImage || NO_IMAGE}
-                      alt="Profile"
-                      style={{ width: "300px", borderRadius: "100%" }}
-                    />
-                  </Col>
-                  <Col span={16}>
-                    <Title level={3}>Name: {userData?.name}</Title>
-                    <Text strong>Email:</Text> <Text>{userData?.email}</Text>
-                    <br />
-                    <Text strong>Phone:</Text>{" "}
-                    <Text>{userData?.phoneNumber}</Text>
-                    <br />
-                    {userData?.address && (
-                      <>
-                        <Text strong>Address:</Text>{" "}
-                        <Text>{userData?.address}</Text>
-                        <br />
-                      </>
-                    )}
-                    {userData?.gender && (
-                      <>
-                        <Text strong>Gender:</Text>{" "}
-                        <Text>{userData?.gender}</Text>
-                        <br />
-                      </>
-                    )}
-                    {userData?.dateOfBirth && (
-                      <>
-                        <Text strong>Date of Birth:</Text>{" "}
-                        <Text>{userData?.dateOfBirth}</Text>
-                        <br />
-                      </>
-                    )}
-                  </Col>
-                </Row>
-              </Space>
-            </div>
-          </div>
-        </div>
-      )} */}
+    
       <UserProfile userData={data} />
       {/* {
         data?.role === "seller" &&
