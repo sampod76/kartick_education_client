ad37c0fce69c38026c7eb5389199b96b2736d321
sarwar-asik <sarwarasik@gmail.com>
2024-03-12 21:27:21 +0600

    about: side sabah image added

--- src/components/Home/Banner&hero/BannerAbout.tsx
+++ src/components/Home/Banner&hero/BannerAbout.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 export default function BannerAbout() {
     return (
-        <div className="-mt-[5px] ">
-            <Image alt="" src={"/banner/aboutBanner.png"} className="-z-10 w-[100vw] h-[45vh] lg:h-[100vh]  -mt-[6rem]"
+        <div className="-mt-[10px] ">
+            <Image alt="" src={"/banner/aboutBanner.png"} className="-z-10 w-[100vw] h-[45vh] lg:h-[60vh]  -mt-[6rem]"
                 width={3200}
                 height={2500} />
         </div>
